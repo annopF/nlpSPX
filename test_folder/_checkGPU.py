8535402a07bd3220c7080c183abeab4c035efe3d
OMEN\Annop P <annop20000828@gmail.com>
2022-10-07 17:03:17 +0700

    add title line6

--- test_folder/_checkGPU.py
+++ test_folder/_checkGPU.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from tensorflow.python.client import device_lib
 import torch
 
-
+print("GPU device info:")
 
 print("------------------------------------------------ PyTorch")
 print("PyTorch GPU status: ",torch.cuda.is_available())
@@ -13,3 +13,4 @@ print("Tensorflow GPU status: ", tf.test.is_built_with_cuda)
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')),"\n")
 
 print("GPU name: ",tf.test.gpu_device_name())
+
