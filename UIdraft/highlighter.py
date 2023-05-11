0b8fc4663b4fc3181f8ec8ee88efd7a42b5d7b20
Maxam <thanapatr.maxx@gmail.com>
2023-05-11 12:23:15 +0700

    Fix thread on highlight click missing arg

--- UIdraft/highlighter.py
+++ UIdraft/highlighter.py
@@ -13,7 +13,7 @@ def findtext_inthebox(textbox, suggestionbox, target, parser):
     # List of inp according to textbox 'lines'
     inp = []
 
-    textbox.tag_bind("highlight", "<Button-1>", lambda event: threading.Thread(target = suggestor.on_highlight_click, args=(suggestionbox, event, parser)).start())
+    textbox.tag_bind("highlight", "<Button-1>", lambda event: threading.Thread(target = suggestor.on_highlight_click, args=(textbox, suggestionbox, event, parser)).start())
     textbox.tag_remove("highlight", 1.0, "end-1c")
 
     textbox.tag_remove("highlight-clicked", 1.0, "end-1c")
