1cb9800d606c5368e227caef4d953070440b6e78
Maxam <thanapatr.maxx@gmail.com>
2023-05-11 17:57:47 +0700

    test

--- UIdraft/test/test.py
+++ UIdraft/test/test.py
@@ -1,25 +1,91 @@
 from tkinter import *
+import tkinter
 
 root = Tk()
+root.title("Draft GUI")
+root.geometry("200x400")
+root.resizable(width=False, height=False)
 frame = Frame(root)
-frame.pack()
+# frame.bindtags((str(frame), str(root), "all"))
 
-bottomframe = Frame(root)
-bottomframe.pack( side = BOTTOM )
+frame.pack(expand=True, fill='both', side="left")
+frame.pack_propagate(False)
+# print(get_screensize)
 
-sideframe = Frame(root)
-sideframe.pack( side = RIGHT )
+frame.update()
 
-redbutton = Button(frame, text="Red", fg="red")
-redbutton.pack( side = LEFT)
+button_row = {}
+# for x in range(1, 10):
+#     button_row["row{0}".format(x)] = "Hello"
+print(button_row)
+print(type(button_row))
 
-greenbutton = Button(sideframe, text="green", fg="green")
-greenbutton.pack( side = LEFT , fill=BOTH)
 
-bluebutton = Button(frame, text="Blue", fg="blue")
-bluebutton.pack( side = LEFT )
+# btn.pack()
+# btn.update()
+# print(btn.winfo_width())
 
-blackbutton = Button(bottomframe, text="Black", fg="black")
-blackbutton.pack( side = BOTTOM)
+texts = ["one", "two", "three","four", "five", "six","seven", "eight", "nine","ten", "eleven", "twelve", "capybara"]
+
+def print_text(text):
+    print(text)
+
+
+def clear_last(obj):
+    last = obj.winfo_children()[-1]
+    last.destroy()
+
+
+# For inter_values
+def destroy_subrow_btn(fr):
+    for row in fr:
+        for widget in row.winfo_children():
+            if isinstance(widget, tkinter.Button):
+                widget.destroy()
+    return
+
+
+def wrap_btn_place(parent, text_list):
+    width = parent.winfo_width()
+    current_row = 0
+    current_column = 0
+
+    available_width = width
+
+    button_row["row{0}".format(current_row)] = Frame(parent, bg="green")
+    button_row["row{0}".format(current_row)].grid(sticky="we")
+    for text in text_list:
+        text_btn = Button(button_row["row{0}".format(current_row)], text=f"{text}", command=lambda x=text: print_text(x))
+        text_btn.grid(row=0, column=current_column)
+        text_btn.update()
+        using_width = text_btn.winfo_width()
+        if available_width < using_width:
+            clear_last(button_row["row{0}".format(current_row)])
+            available_width = width
+            current_row += 1
+            button_row["row{0}".format(current_row)] = Frame(parent, bg="blue")
+            text_btn = Button(button_row["row{0}".format(current_row)], text=f"{text}",
+                              command=lambda x=text: print_text(x))
+            button_row["row{0}".format(current_row)].grid(sticky="we")
+            current_column = 0
+            text_btn.grid(row=0, column=current_column)
+            available_width -= using_width
+            print("placed button at: ", current_row, current_column)
+            print("Available: ", available_width)
+            current_column += 1
+        else:
+            text_btn.grid(row=0, column=current_column)
+            available_width -= using_width
+            print("placed button at: ", current_row, current_column)
+            print("Available: ", available_width)
+            current_column += 1
+
+        # print(available_width)
+
+# for n in range(0, 5):
+#     Button(frame, text="Capybara").pack()
+
+
+wrap_btn_place(frame, texts)
 
 root.mainloop()
